c52eeab3337e999e3908bf39ef7918c2fbbf961f
Burawi <burawi.ba@gmail.com>
2022-04-15 15:51:20 +0100

    add Arab numerals

--- strFunctions.js
+++ strFunctions.js
@@ -1,6 +1,16 @@
+const ExtensionUtils = imports.misc.extensionUtils;
+const extension = ExtensionUtils.getCurrentExtension();
+const convenience = extension.imports.convenience;
+
+const Schema = convenience.getSettings(extension, 'hijri-calendar');
+
 function format(str) {
     let enums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let hnums = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+
+    if (Schema.get_boolean('arab-numerals')) {
+        return `${str}`
+    }
 
     return replace(enums, hnums, str);
 }
